8c02c8f8eff7d7ae064ff3341f3b26dbb9f5e50a
DaffaMalik12 <dmalikakram@gmail.com>
2023-10-09 17:12:41 +0700

    first commit

---
+++ NaryTreeTraversal.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// Kelas untuk merepresentasikan node dalam pohon N-ary
+class TreeNode {
+    int data;
+    List<TreeNode> children;
+
+    TreeNode(int data) {
+        this.data = data;
+        children = new ArrayList<>();
+    }
+}
+
+public class NaryTreeTraversal {
+
+    // Fungsi untuk mencetak pohon N-ary secara PreOrder
+    public static void preOrderTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            for (TreeNode child : root.children) {
+                preOrderTraversal(child);
+            }
+        }
+    }
+
+    // Fungsi untuk mencetak pohon N-ary secara PostOrder
+    public static void postOrderTraversal(TreeNode root) {
+        if (root != null) {
+            for (TreeNode child : root.children) {
+                postOrderTraversal(child);
+            }
+            System.out.print(root.data + " ");
+        }
+    }
+
+    // Fungsi untuk mencetak pohon N-ary secara LevelOrder
+    public static void levelOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            System.out.print(current.data + " ");
+
+            for (TreeNode child : current.children) {
+                queue.offer(child);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+
+        root.children.add(node2);
+        root.children.add(node3);
+        node2.children.add(node4);
+        node2.children.add(node5);
+
+        System.out.println("PreOrder traversal: ");
+        preOrderTraversal(root);
+        System.out.println();
+
+        System.out.println("PostOrder traversal: ");
+        postOrderTraversal(root);
+        System.out.println();
+
+        System.out.println("LevelOrder traversal: ");
+        levelOrderTraversal(root);
+        System.out.println();
+    }
+}
---
+++ binarytree.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Struktur untuk merepresentasikan node dalam pohon biner
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Fungsi untuk membuat node baru dalam pohon biner
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Fungsi untuk mencetak pohon biner secara PreOrder
+void preOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data); // Cetak akar
+        preOrderTraversal(root->left); // Rekursif ke anak kiri
+        preOrderTraversal(root->right); // Rekursif ke anak kanan
+    }
+}
+
+// Fungsi untuk mencetak pohon biner secara InOrder
+void inOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left); // Rekursif ke anak kiri
+        printf("%d ", root->data); // Cetak akar
+        inOrderTraversal(root->right); // Rekursif ke anak kanan
+    }
+}
+
+// Fungsi untuk mencetak pohon biner secara PostOrder
+void postOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        postOrderTraversal(root->left); // Rekursif ke anak kiri
+        postOrderTraversal(root->right); // Rekursif ke anak kanan
+        printf("%d ", root->data); // Cetak akar
+    }
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("PreOrder traversal: ");
+    preOrderTraversal(root);
+    printf("\n");
+
+    printf("InOrder traversal: ");
+    inOrderTraversal(root);
+    printf("\n");
+
+    printf("PostOrder traversal: ");
+    postOrderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
